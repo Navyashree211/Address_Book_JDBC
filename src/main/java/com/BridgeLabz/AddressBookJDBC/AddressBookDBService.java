c046bb7a2ac735e4fee76dda0254c8529ed34dc2
unknown <navyas211@gmail.com>
2021-11-25 15:01:13 +0530

    [NAVAYA]ADDED: Ability to Retrieve number of contacts in the Database by city or state

--- src/main/java/com/BridgeLabz/AddressBookJDBC/AddressBookDBService.java
+++ src/main/java/com/BridgeLabz/AddressBookJDBC/AddressBookDBService.java
@@ -1,5 +1,5 @@
 /*
- * UC18: Ability to Retrieve Contact from the database that were added in a particular period .
+ * UC19: Ability to Retrieve number of contacts in the Database by city or state .
  * 
  * @author : Navaya Shree
  * @since :  22-11-2021
@@ -10,104 +10,117 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AddressBookDBService {
 	static PreparedStatement pstmt;
 	static Connection con;
 	static ResultSet rs;
 
-	public static List<ContactPerson> fetchContactPersonList() throws Exception {
+	public static void  fetchContactPersonListByCity() throws Exception {
 
-		List<ContactPerson> details = new ArrayList<>();
-	
-	Connection con = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
-
-	String qry = "select * from address_book.address_books   where date between Cast('2020-01-01' as date) and date (now())";
+		int count = 0;
+			
+	String qry = " select count(*) from address_book.address_books where city = 'udaipur'; ";
 	try {
 		con = ConnectionDB.createCP();
 
 		pstmt = con.prepareStatement(qry);
 
-		rs = pstmt.executeQuery();
+		rs = pstmt.executeQuery();   
 		
-		System.err.println("firstName-> " + "lastName-> " +  "address-> " + "    city-> "
-				+ "state-> " + " zip-> " + "phoneNumber-> " + "date -> " );
+		rs.next();
+		count = rs.getInt(1);
+		System.out.println("Count of City is : " + count);
 
-		while (rs.next()) {   
-			ContactPerson info = new ContactPerson();
-			
-			String firstName = rs.getString(1);
-			info.setFirstName(firstName);
-			
-			String lastName = rs.getString(2);
-			info.setLastName(lastName);
-			
-			String address = rs.getString(3);
-			info.setAddress(address);
-			
-			String city = rs.getNString(4);
-			info.setCity(city);
-			
-			String state = rs.getString(5);
-			info.setState(state);
-			
-			long zip = rs.getLong(6);
-			info.setZip(zip);
-			
-			long phoneNumber = rs.getLong(7);
-			info.setPhoneNumber(phoneNumber);
-			
-			String date = rs.getString(8);
-			info.setDate(date);
-			
-			details.add(info);
-			System.out.println(info.toString());
-			System.out.println();
-
+		}catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
 		}
 
-	} catch (ClassNotFoundException | SQLException e) {
-		e.printStackTrace();
-	}
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 
-	finally {
-		if (rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
 			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
 
-		}
-		if (pstmt != null) {
-			try {
-				pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 
-			} catch (SQLException e) {
-				e.printStackTrace();
 			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 
-		}
-		if (con != null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
 			}
-
+			System.out.println("Closed All Resources");
 		}
-		System.out.println("Closed All Resources");
-	}
-	return details;
-
-}
 	
+	}
+	
+	public static void  fetchContactPersonListByState() throws Exception {
+
+		int count = 0;
+			
+	String qry = " select count(*) from address_book.address_books where state = 'USA'; ";
+	try {
+		con = ConnectionDB.createCP();
+
+		pstmt = con.prepareStatement(qry);
+
+		rs = pstmt.executeQuery();   
+		
+		rs.next();
+		count = rs.getInt(1);
+		System.out.println("Count of State is : " + count);
+
+		}catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			System.out.println("Closed All Resources");
+		}
+	
+	}
+
 	public static void main(String[] args) throws Exception {
-		AddressBookDBService.fetchContactPersonList();
+		
+		AddressBookDBService.fetchContactPersonListByCity();
+		AddressBookDBService.fetchContactPersonListByState();
 	}
-	
 }
